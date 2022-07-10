71a52a918ec2f70a6f1377a3d84804d6594ab2fc
Thiago Oliveira <thiceconelo@gmail.com>
2022-07-10 15:02:53 -0300

    Creation of method to insert names of marketplaces on db by API.

--- marketplaces/config.py
+++ marketplaces/config.py
@@ -10,6 +10,8 @@ from marketplaces.spiders.shopsmiles import ShopsmilesSpider
 from marketplaces.spiders.redeem import ReedemSpider
 from marketplaces.spiders.shoptudoazul import ShoptudoazulSpider
 
+from loguru import logger as log
+
 SPIDERS = [
     AmericanasSpider,
     CasasbahiaSpider,
@@ -22,7 +24,7 @@ SPIDERS = [
     ShopsmilesSpider,
     ReedemSpider,
     ShoptudoazulSpider
-    ]
+]
 
 KEYWORDS = [
     # 'dell g15 -p 4000',
@@ -33,3 +35,28 @@ KEYWORDS = [
 ]
 
 VALIDATE_FREIGHT = False
+URL_BASE_API = 'http://localhost:8000/api/v1/'
+
+
+def create_marketplaces_on_db():
+    import re
+    import requests
+    from http import HTTPStatus
+
+    for spider in SPIDERS:
+        remove = 'Spider'
+        name = re.sub(remove, '', spider.__name__)
+        log.info(f'Criando {name} no banco de dados.')
+
+        url = f'{URL_BASE_API}lojas'
+        result = requests.post(url, json={'nome': name})
+
+        if result.status_code == HTTPStatus.CREATED:
+            log.info(f'{name} criado com sucesso!')
+        else:
+            log.error(f'{name} ocorreu um erro ao criar!')
+            lof.error(f'{result.text}')
+
+
+if __name__ == '__main__':
+    create_marketplaces_on_db()
