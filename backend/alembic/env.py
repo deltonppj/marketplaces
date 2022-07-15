491905974aefed298a46423ef9733a96cd822250
Thiago Oliveira <thiceconelo@gmail.com>
2022-07-15 13:48:54 -0300

    Env alembic

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -9,10 +9,6 @@ from sqlalchemy.ext.asyncio import AsyncEngine
 
 from alembic import context
 
-from dotenv import load_dotenv
-
-load_dotenv()
-
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
